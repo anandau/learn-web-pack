7b9c829b5ad7c1f6977ede8fefd6f79dc81d2635
Anand <anand@webtechstreet.com>
2020-05-06 12:21:44 +0530

    File loader done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import './style.css';
+import Icon from './logo.png';
 
 function component() {
     const element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+
+    const icon = new Image();
+    icon.src = Icon; 
+    icon.width = '200';
+    
+    element.appendChild(icon);
   
     return element;
   }

87c4087ee9b89ab8dcb478c9fe96b0c5f7c2c683
Anand <anand@webtechstreet.com>
2020-05-06 12:26:59 +0530

    Code cleanup

--- src/index.js
+++ src/index.js
@@ -1,19 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './logo.png';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
-
-    const icon = new Image();
-    icon.src = Icon; 
-    icon.width = '200';
-    
-    element.appendChild(icon);
   
     return element;
   }
